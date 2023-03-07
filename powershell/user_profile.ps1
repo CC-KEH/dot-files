b27a73a755d99963704cef50545d1a5d1ae1377e
CC-KEH <CC-KEH@github.com>
2023-03-07 14:52:15 +0530

    files added

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,6 @@
+# Prompt
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_shell.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\agnoster.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\the-unnamed.omp.json" | Invoke-Expression
+Import-Module Terminal-Icons
